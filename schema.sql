3b6d80a6a9692996ac119cb7a780c7cdf42f37d7
Sri Lakshmi <srilakshmiinala9@gmail.com>
2026-04-24 09:29:26 -0500

    added schema

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
+CREATE DATABASE HealthcareReadmission;
+GO
 
+USE HealthcareReadmission;
+GO
+
+CREATE TABLE Patients (
+    PatientID INT PRIMARY KEY,
+    PatientName VARCHAR(100),
+    Gender VARCHAR(10),
+    Age INT,
+    City VARCHAR(50)
+);
+
+CREATE TABLE Admissions (
+    AdmissionID INT PRIMARY KEY,
+    PatientID INT,
+    AdmitDate DATE,
+    DischargeDate DATE,
+    Diagnosis VARCHAR(100),
+    Readmitted30Days VARCHAR(3),
+    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID)
+);
